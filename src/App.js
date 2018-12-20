7ccec8a0fdac2765644e5023a14aac3fa9981d92
zhouxiaoan <13598091228@139.com>
2018-12-20 14:37:23 +0800

    增加配置文件

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import UnauthorizedLayout from './components/UnauthorizedLayout'
+import AuthorizedRoute from './components/AuthorizedRoute'
+import PrimaryLayout from './components/PrimaryLayout'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/auth" component={UnauthorizedLayout}> </Route>
+            <AuthorizedRoute path="/app" component={PrimaryLayout} />
+            <Redirect to="/auth" />
+          </Switch>
+        </BrowserRouter>
+      
     );
   }
 }
